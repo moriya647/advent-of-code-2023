d26f1cd850a323f8d482ab88182046dede9622ed
a.eren <adem.eren0598@gmail.com>
2023-12-21 18:20:39 +0100

    add day_3 part_one

--- src/day_2/part_two.rs
+++ src/day_2/part_two.rs
@@ -7,12 +7,16 @@ fn part_two() {
     let input = fs::read_to_string("src/day_2/full_input.txt").unwrap();
     let mut power = 0;
 
-    for game in input.lines().map(|line| Game { record: line }) {
+    for game in input.lines().map(to_game()) {
         let max_set = game.parse_max_set();
         power += max_set.green * max_set.red * max_set.blue;
     }
 
     println!("{power}");
+}
+
+fn to_game() -> fn(&str) -> Game {
+    |line| Game { record: line }
 }
 
 #[cfg(test)]
